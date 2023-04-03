516bfde156378482d4548eb8185bd36106d887b5
Nicholas Gao <nicholas.gao@tum.de>
2023-04-03 11:42:38 +0200

    specify important package versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = [
     'numpy',
     'scipy',
     'tqdm',
-    'jax',
+    'jax>=0.4, <0.5',
     'flax',
     'jraph',
     'pyscf',
@@ -12,9 +12,9 @@ install_requires = [
     'sympy',
     'matplotlib',
     'uncertainties',
-    'seml',
-    'seml_logger @ git+https://github.com/n-gao/seml_logger.git#egg=seml_logger',
-    'sacred'
+    'seml>=0.3, <0.4',
+    'seml_logger @ git+https://github.com/n-gao/seml_logger.git@5eb5f19d1694d858af0c727291284e16a8d190e1#egg=seml_logger',
+    'sacred>=0.8.4, <0.9'
 ]
 
 
